5bb6c3d4da81c90742eb14cd5fd9b3cebada2c4e
Anelvin <amejiaroque@gmail.com>
2021-07-29 01:47:57 +0200

    Get token on methos login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import SignIn from './views/signin/SignIn.jsx';
@@ -6,13 +6,15 @@ import SignUp from './views/signup/SignUp.jsx';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' exact><Redirect to="/signin" /></Route>
-        <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
-      </Switch>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route path='/' exact><Redirect to="/signin" /></Route>
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+        </Switch>
+      </Router>
+    </React.StrictMode>
   );
 }
 
--- src/views/signup/signUp.jsx
+++ src/views/signup/signUp.jsx
@@ -1,24 +1,75 @@
-import React, { useState } from 'react';
-import { TextField, Button } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { TextField, Button, CircularProgress, Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import environment from '../../environments/environment';
+import axios from 'axios';
+
+function Alert(props){
+    return <MuiAlert elevation={6} variant="filled" {...props} />
+}
 
 function SignUp(){
     
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
     const [confirmPassword, setConfirmPassword] = useState(null); 
+    const [url, setUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [severity, setSeverity] = useState('success');
+    const [message, setMessage] = useState(null);
+
+    useEffect(() => {
+        setUrl(environment.apiUrl);
+    })
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway'){
+            return;
+        }
+        setOpen(false);
+    }
+
+    const handleError = () => {
+        setSeverity('error');
+        setLoading(false);
+        setOpen(true);
+        setMessage('Error al conectar con el servidor');
+    }
 
     const createAccount = () => {
+        setLoading(true);
         const data = {
             email,
             password,
             confirmPassword
         }
-        console.log(data);
+        if(password === confirmPassword){
+            axios.post(url + 'auth/signup', data)
+                .then(result => {
+                    if(result.data.token){
+                        console.log(result);
+                        setLoading(false);
+                        setOpen(true);
+                        setMessage('Usuario registrado!');
+                        setSeverity('success');
+                    } else {
+                        handleError();
+                    }
+                })
+        } else {
+            handleError()
+        }
     }
 
     return (
         <div className="container-signin">
             <div className="form-signin">
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity={severity}>
+                        { message }
+                    </Alert>
+                </Snackbar>
                 <img src="images/jira-software.png" alt="" />
                 <h2>Crear cuenta</h2>
                 <div className="container-input">
@@ -52,12 +103,17 @@ function SignUp(){
                     />
                 </div>
                 <div className="container-input">
-                    <Button 
-                        variant="contained" 
-                        color="primary"
-                        fullWidth
-                        onClick={createAccount}
-                    >Crear cuenta</Button>
+                    {
+                        loading === true ? 
+                            <CircularProgress /> 
+                        : 
+                            <Button 
+                                variant="contained" 
+                                color="primary"
+                                fullWidth
+                                onClick={createAccount}
+                            >Crear cuenta</Button>
+                    }
                 </div>
                 <div className="container-input">
                     <Button 
