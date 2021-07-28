153a6e805c11148afbbd13a1863161f55c68febf
Anelvin <amejiaroque@gmail.com>
2021-07-28 23:53:32 +0200

    View signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import signIn from './views/signin/signIn.jsx';
+import signUp from './views/signup/signUp.jsx';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact component={signIn} />
+        <Route path='/' exact><Redirect to="/signin" /></Route>
+        <Route path='/signin' component={signIn} />
+        <Route path='/signup' component={signUp} />
       </Switch>
     </Router>
   );
