dc6fe73279d9392ec0bf2a71a2f3cfebdea4b3d3
Anelvin <amejiaroque@gmail.com>
2021-08-11 01:37:48 +0200

    Table with pointer and paginate

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,6 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { TablePagination } from '@material-ui/core';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -31,36 +32,71 @@ const useStyles = makeStyles({
   table: {
     minWidth: 700,
   },
+  row: {
+    cursor: 'pointer'
+  }
 });
+
+const handleClick = (row) => {
+  console.log(row);
+}
 
 export default function CustomizedTables(props) {
   const classes = useStyles();
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-              {props.headers.map((item, index) => {
-                  return (
-                      <StyledTableCell align="left" key={index}>{item}</StyledTableCell>
-                  )
-              })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.dataColumn.map((row) => (
-            <StyledTableRow key={row.id}>
-              <StyledTableCell component="th" scope="row">
-                {row.id}
-              </StyledTableCell>
-              <StyledTableCell align="left">{row.name}</StyledTableCell>
-              <StyledTableCell align="left">{row.description}</StyledTableCell>
-              <StyledTableCell align="left">{row.createdAt}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer component={Paper}>
+        <Table stickyHeader className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+                {props.headers.map((item, index) => {
+                    return (
+                        <StyledTableCell align="left" key={index}>{item}</StyledTableCell>
+                    )
+                })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.dataColumn.map((row) => (
+              <StyledTableRow className={classes.row} key={row.id} onClick={ ()=> handleClick(row)}>
+                <StyledTableCell component="th" scope="row">
+                  {row.id}
+                </StyledTableCell>
+                <StyledTableCell align="left">{row.name}</StyledTableCell>
+                <StyledTableCell align="left">{row.description}</StyledTableCell>
+                <StyledTableCell align="left">{row.createdAt}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        labelDisplayedRows={
+          ({ from, to, count }) => {
+            return '' + from + '-' + to + ' de ' + count
+          }
+        }
+        labelRowsPerPage='Filas por página'
+        component="div"
+        count={props.dataColumn.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
   );
 }
