5dccf1540d59b8c74bcad218b2127b6973e7b4f3
Anelvin <amejiaroque@gmail.com>
2021-08-19 00:41:55 +0200

    Section 3 home

--- src/views/public/Public.jsx
+++ src/views/public/Public.jsx
@@ -15,12 +15,17 @@ const useStyles = makeStyles((theme) => ({
         maxWidth: '1200px',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: '50px',
+    },
+    boxShadow: {
+        boxShadow: '6px 30px 30px rgb(23 43 77 / 10%)'
     },
     column1:{
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginBottom: '25px'
     },
     oneColumn: {
         textAlign: 'center',
@@ -41,6 +46,22 @@ const useStyles = makeStyles((theme) => ({
     },
     img: {
         width: '800px'
+    },
+    columnCenter: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    rowCenter: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    marginHorizontal: {
+        margin: '0 10px'
+    },
+    fontSize: {
+        fontSize: '1.25rem'
     }
 }));
 
@@ -60,6 +81,25 @@ const Public = () => {
                 <h1 style={{maxWidth: '700px'}}>Los mejores equipos de software entregan pronto y frecuentemente</h1>
                 <p style={{maxWidth: '700px', marginTop: '25px', fontSize:'20px'}}>Jira Software está diseñado para que todos los miembros de tu equipo de software puedan planificar,
                 supervisar y publicar software de gran calidad.</p>
+            </div>
+            <div className={classes.twoColumn}>
+                <div className={classes.boxShadow}>
+                    <img className={classes.img} src="./images/home2.png" alt="Imagen 1" />
+                </div>
+                <div className={classes.columnCenter} style={{marginLeft: '20px'}}>
+                    <h3>Planifica</h3>
+                    <p style={{marginTop: '25px', fontSize:'20px'}}>Crea historias de usuario e incidencias, planifica sprints y distribuye tareas entre tu equipo de software.</p>
+                    <h3 style={{marginTop: '25px'}}>Supervisa</h3>
+                    <p style={{marginTop: '25px', fontSize:'20px'}}>Prioriza y analiza el trabajo de tu equipo en su contexto y con una completa visibilidad.</p>
+                </div>
+            </div>
+            <div className={classes.twoColumn}>
+                <div className={classes.marginHorizontal} style={{width: '33%'}}>
+                    <h1>Elige un flujo de trabajo o crea uno</h1>
+                </div>
+                <div className={classes.marginHorizontal} style={{width: '33%'}}>
+                    <p className={classes.fontSize}>Cada equipo cuenta un proceso único para lanzar software. Utiliza un workflow predefinido o crea uno adaptado a la forma de trabajar de tu equipo.</p>
+                </div>
             </div>
         </div>
     )
